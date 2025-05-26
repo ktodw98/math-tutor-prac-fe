08e7d8098183b7a4d43c52cdcf62ba32835d0b39
ktodw98 <infuilamust17@gmail.com>
2025-05-27 00:43:37 +0900

    make hook:useGetMessage

--- math-tutor-prac/src/hook/useGetMessage.tsx
+++ math-tutor-prac/src/hook/useGetMessage.tsx
@@ -1,14 +1,39 @@
 import axios from "axios";
 import { useState, useEffect } from 'react';
 
+interface ApiResponse {
+    message: string;
+}
+
 function useGetMessage ( url:string ) {
-    const [message, setMessage] = useState('');
+    const [apiResponse, setApiResponse] = useState<ApiResponse | null>(null);
+    const [error, setError] = useState<string | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
 
     useEffect(() => {
-        axios.get(url).then((res) => setMessage(res.data));
+        if (!url) {
+            setLoading(false);
+            return;
+        }
+
+        setLoading(true);
+        axios.get<ApiResponse>(url)
+            .then((res) => {
+                setApiResponse(res.data)
+                setError(null);
+            })
+            .catch(err => {
+                console.log("Error fetching data: ", err);
+                setError(err.message || "Failed to fetch data");
+                setApiResponse(null);
+            })
+            .finally(() => {
+                setLoading(false);
+            })
+
     }, [url]);
 
-    return message
+    return { data: apiResponse, loading, error };
 }
 
 export default useGetMessage
--- math-tutor-prac/src/pages/home.tsx
+++ math-tutor-prac/src/pages/home.tsx
@@ -3,13 +3,20 @@ import useGetMessage from '../hook/useGetMessage';
 
 
 function Home() {
-    const url = "https://127.0.0.1/8000"
+    const url = "http://127.0.0.1:8000/home/"
 
-    const word = useGetMessage(url)
+    const { data, loading, error } = useGetMessage(url)
+
+    if ( loading ) {
+        return <p>Loading...</p>
+    }
+    if (error) {
+        return <p>Error: { error }</p>
+    }
     return (
         <>
             <button>받아오기</button>
-            <p>{word}</p>
+            <p>{ data? data.message : 'no message'}</p>
         </>
     )
 
